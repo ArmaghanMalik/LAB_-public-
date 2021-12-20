3b11cc828a3c0089360bdf8b35ed8a667ca75d46
ArmaghanMalik <armaghan_ali@hotmail.com>
2021-12-20 17:21:33 +0500

    added print

--- LAB.py
+++ LAB.py
@@ -2,7 +2,7 @@ print("edited by user 3
       ")
       print("HELLO")
       
-      
+      print("abcdef")
 string = "abcdeee"
 decrypt=""
 alphabet_dictionary = {'a':1,'b':2,'c':3,'d':4, 'e':5}  #, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10}
