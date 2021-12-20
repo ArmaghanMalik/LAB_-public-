f0360a83ec8af15d6c142078c34258a667c258fd
ArmaghanMalik <96419833+ArmaghanMalik@users.noreply.github.com>
2021-12-20 17:04:49 +0500

    added print

--- LAB.py
+++ LAB.py
@@ -1,5 +1,6 @@
 print("edited by user 2
       ")
+      print("HELLO")
 string = "abcdeee"
 decrypt=""
 alphabet_dictionary = {'a':1,'b':2,'c':3,'d':4, 'e':5}  #, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10}
